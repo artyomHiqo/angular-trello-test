cc07a52750fbbfb25aed742a9cd02cc6682cd70e
Artem Zborovsky <artem.zborovsky@hiqo-solutions.com>
2020-09-21 19:34:30 +0300

    HEDU-4896: added API services

---
+++ src/app/core/services/api.service.ts
@@ -0,0 +1,58 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+
+import { APIUrl } from '../constants';
+
+// import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private options: {
+    headers: HttpHeaders;
+  };
+
+  constructor(private http: HttpClient) {
+    const token = localStorage.getItem('token') || '';
+    this.options = {
+      headers: new HttpHeaders({
+        authorization: token
+      })
+    };
+  }
+
+  setToken(token: string): void {
+    this.options.headers.set('authorization', token);
+    localStorage.setItem('token', token);
+  }
+
+  resetToken(token: string): void {
+    this.options.headers.delete('authorization', token);
+    localStorage.removeItem('token');
+  }
+
+  postWithoutToken<T>(path: string, body: any): Promise<T> {
+    return this.http.post(`${APIUrl}/${path}`, body)
+      .pipe(map((response: any) => {
+        if (response.success && response.data.token) {
+          this.setToken(response.data.token);
+        }
+        return response.data as T;
+      }))
+      .toPromise();
+  }
+
+  post<T>(path: string, body: any): Promise<T> {
+    return this.http.post(`${APIUrl}/${path}`, body, this.options)
+      .pipe(map((response: any) => response.data as T))
+      .toPromise();
+  }
+
+  get<T>(path: string): Promise<T> {
+    return this.http.get(`${APIUrl}/${path}`, this.options)
+      .pipe(map((response: any) => response.data as T))
+      .toPromise();
+  }
+}
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'app/core/services/auth.service';
 import { Observable } from 'rxjs';
-
-import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-header',
